1ba23938c6f5ff9714a61400afcce8d0418801cd
andreapalmieri01 <andrea.palmieri2001@gmail.com>
2022-12-27 13:08:33 +0100

    nuova classe

--- Ingegeria-del-Software/src/main/java/it/unicam/cs/ids/loyalty/Cliente.java
+++ Ingegeria-del-Software/src/main/java/it/unicam/cs/ids/loyalty/Cliente.java
@@ -1,4 +1,13 @@
 package it.unicam.cs.ids.loyalty;
 
 public class Cliente {
+    private long id;
+
+    public Cliente(long id){
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
 }
