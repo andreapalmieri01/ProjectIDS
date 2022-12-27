73b005797250b7bde9ad1275607656972e4a1545
andreapalmieri01 <andrea.palmieri2001@gmail.com>
2022-12-27 11:42:17 +0100

    Initial commit

---
+++ Ingegeria-del-Software/src/main/java/it/unicam/cs/ids/loyalty/LoyaltyProjectApplication.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.ids.loyalty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoyaltyProjectApplication.class, args);
+	}
+
+}
